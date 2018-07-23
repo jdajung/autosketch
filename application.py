83e27f5d1599a9d650ddafd051bd653f6f823637
Rahul Iyer <rahuliyer@v1020-wn-38-188.campus-dynamic.uwaterloo.ca>
2018-07-23 17:28:02 -0400

    fixed add blob

--- application.py
+++ application.py
@@ -145,16 +145,16 @@ def satisfy_check(level,x,y):
 
     return True
 
-def out_contour(x, y, contour):
+def out_contour(x, y, contour,radius_multiplier=3):
     global drawRadius
-    ret = cv2.pointPolygonTest(contour, (x, y), True)
-    if ret > -(5*drawRadius):
+    ret = cv2.pointPolygonTest(contour, (y, x), True)
+    if ret > -(radius_multiplier*drawRadius):
         return False
 
     return True
 
-def in_contour( x, y, contour, radius_multiplier=4):
-    ret = cv2.pointPolygonTest(contour, (x, y), True)
+def in_contour( x, y, contour, radius_multiplier=3):
+    ret = cv2.pointPolygonTest(contour, (y, x), True)
     if ret < (radius_multiplier*drawRadius):
         # print("Failed in in_contour")
         return False
@@ -166,17 +166,17 @@ def add_blob(part):
     radius = 5
     colour = BLACK
 
-    extreme_top = tuple(part.contour[part.contour[:, :, 0].argmin()][0])
-    extreme_bottom = tuple(part.contour[part.contour[:, :, 0].argmax()][0])
-    extreme_left = tuple(part.contour[part.contour[:, :, 1].argmin()][0])
-    extreme_right = tuple(part.contour[part.contour[:, :, 1].argmax()][0])
+    extreme_left = tuple(part.contour[part.contour[:, :, 0].argmin()][0]) # extreme_left
+    extreme_right = tuple(part.contour[part.contour[:, :, 0].argmax()][0]) # extreme_right
+    extreme_top = tuple(part.contour[part.contour[:, :, 1].argmin()][0]) # extreme_top
+    extreme_bottom = tuple(part.contour[part.contour[:, :, 1].argmax()][0]) # extreme_bottom
     print(extreme_top,extreme_bottom,extreme_left,extreme_right)
-    # cv2.circle(img,extreme_left,1,(0,0,255),-1)
+    # cv2.circle(img,extreme_left,1,(255,0,0),-1)
     # cv2.circle(img,extreme_right,1,(0,0,255),-1)
-    # cv2.circle(img,extreme_top,1,(0,0,255),-1)
-    # cv2.circle(img,extreme_bottom,1,(0,0,255),-1)
-    range_in_x = (extreme_top[0],extreme_bottom[0])
-    range_in_y = (extreme_left[1],extreme_right[1])
+    # cv2.circle(img,extreme_top,1,(0,255,0),-1)
+    # cv2.circle(img,extreme_bottom,1,(255,0,255),-1)
+    range_in_x = (extreme_top[1],extreme_bottom[1])
+    range_in_y = (extreme_left[0],extreme_right[0])
     count = 0
     max_count = 100
 
@@ -186,7 +186,7 @@ def add_blob(part):
 
         if satisfy_check(part,sampled_x,sampled_y) and in_contour(sampled_x,sampled_y,part.contour):
             print("Drawing on %d %d" % (sampled_x,sampled_y))
-            cv2.circle(img, (sampled_x, sampled_y), radius, colour, -1)
+            cv2.circle(img, (sampled_y, sampled_x), radius, colour, -1)
             # updateEncodings()
             break
         count += 1
